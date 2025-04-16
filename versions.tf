1e068a5a0bf4c4ea2feac412b3779a41830010a8
kpeacocke <krpeacocke@gmail.com>
2025-04-16 21:32:44 +1000

    fix: update GitHub provider source and version constraints in Terraform configuration

--- versions.tf
+++ versions.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     github = {
-      source  = "hashicorp/github"
-      version = ">= 6.0.0"
+      source  = "integrations/github"
+      version = "~> 6.0"
     }
   }
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.5.0"
 }
 
 provider "github" {
